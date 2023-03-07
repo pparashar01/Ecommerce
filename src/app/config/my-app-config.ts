8fe365bd7f66161622a0675338fbe981c9dd55c1
pparashar01 <pparashar01.02.1998@gmail.com>
2023-03-07 15:13:31 +0530

    Add files via upload

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc:{
+    clientId:'0oa8j2cyt9Tpq9TdE5d7',
+    issuer:'https://dev-49420704.okta.com/oauth2/default',
+    redirectUri:'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+
+}
